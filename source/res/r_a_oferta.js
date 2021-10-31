157a8ffaa5eeb33965720c4463a349dc771903f0
luisfernandomc <lmluismorochoc@gmail.com>
2021-10-31 14:14:52 -0500

    registrar ofertas

--- source/res/r_a_oferta.js
+++ source/res/r_a_oferta.js
@@ -82,32 +82,36 @@ function listarOfertas(req, res) {
 //    var idAdministrador = req.body.idAdministrador;
     var desde = req.body.desde;
     var cuantos = req.body.cuantos;
-   
+    var idUsuario = req.body.idUsuario;
+
+
     var criterio = req.body.criterio;
 //    var fInicio = req.body.fInicio;
 //    var fFin = req.body.fFin;
+    var where = ''
+    if (!idUsuario) 
+        where = ' where od.id_ofertante= ' + idUsuario;
 
-//    if (!idAdministrador)
-//        return res.status(400).send({error: 1, param: 'idAdministrador'});
     if (!desde)
         return res.status(400).send({error: 1, param: 'desde'});
     if (!cuantos)
         return res.status(400).send({error: 1, param: 'cuantos'});
 //    if (!criterio)
 //        return res.status(400).send({error: 1, param: 'criterio'});
+    var SQL_OFERTAS = "SELECT od.fecha_creacion ,od.descripcion_actividad,od.titulo, od.numero_minutos,u.idUsuario,u.calificacion, p.nombres, p.apellidos,c.idCategoria,c.categoria FROM bancodt.ofertas_demandas od  inner join usuario u on od.id_ofertante = u.idUsuario inner join persona p on u.id_persona = p.id_persona inner join categoria c on c.idCategoria= od.idCategoria " + where + " order by od.fecha_creacion  desc LIMIT ?, ?;";
 
 
-   
-        cnf.ejecutarResSQL(SQL_OFERTAS, [parseInt(desde), parseInt(cuantos)], function (movmientos) {
-            if (movmientos.length <= 0)
-                return res.status(200).send({en: -1, m: 'Lo sentimos pero no se encuentran movimientos para los parametros enviados.'});
 
-            return res.status(200).send({en: 1, lM: movmientos});
-        }, res);
+    cnf.ejecutarResSQL(SQL_OFERTAS, [parseInt(desde), parseInt(cuantos)], function (movmientos) {
+        if (movmientos.length <= 0)
+            return res.status(200).send({en: -1, m: 'Lo sentimos pero no se encuentran movimientos para los parametros enviados.'});
+
+        return res.status(200).send({en: 1, lM: movmientos});
+    }, res);
 
 
 }
-var SQL_OFERTAS ="SELECT od.fecha_creacion ,od.descripcion_actividad,od.titulo, od.numero_minutos,u.idUsuario, p.nombres, p.apellidos,c.idCategoria,c.categoria FROM bancodt.ofertas_demandas od  inner join usuario u on od.id_ofertante = u.idUsuario inner join persona p on u.id_persona = p.id_persona inner join categoria c on c.idCategoria= od.idCategoria order by od.fecha_creacion  desc LIMIT ?, ?;";
+var SQL_OFERTAS = "SELECT od.fecha_creacion ,od.descripcion_actividad,od.titulo, od.numero_minutos,u.idUsuario,u.calificacion, p.nombres, p.apellidos,c.idCategoria,c.categoria FROM bancodt.ofertas_demandas od  inner join usuario u on od.id_ofertante = u.idUsuario inner join persona p on u.id_persona = p.id_persona inner join categoria c on c.idCategoria= od.idCategoria  order by od.fecha_creacion  desc LIMIT ?, ?;";
 
 
 module.exports = router;
